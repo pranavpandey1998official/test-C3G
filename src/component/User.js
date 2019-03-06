b1a68da9b640ada3805d67e2a6807aad1a956c24
Pranav <pranavpandey1998official@gmail.com>
2019-03-06 16:32:27 +0530

    done required chnages to match new test

--- src/component/User.js
+++ src/component/User.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
- const User = ({ user }) => (
+ class User extends React.Component{ 
+   
+  constructor(props){
+    super(props)
+    this.setPosition=this.setPosition.bind(this)
+  }
+  componentDidMount(){
+    var element=document.getElementById(this.props.user.id)
+    element.parentElement.addEventListener("mouseover",this.setPosition )
+  }
+  componentWillUnmount(){
+    document.getElementById(this.props.user.id).removeEventListener("mouseover",this.setPosition)
+  }
+
+  setPosition= () => {
+    var element=document.getElementById(this.props.user.id)
+    var listContainer=document.getElementById("list-container")
+    element.style.top=element.parentElement.offsetTop+20-listContainer.scrollTop;
+    element.style.left=element.parentElement.offsetWidth+element.parentElement.offsetLeft
+  }
+  
+  render(){
+  var { user } =this.props;
+  return(
   <div className="container">
     <div className="tooltip-container">
       <p className="name">{user.name}</p>
   
-        <div className="tooltip">
+        <div className="tooltip" id={user.id}>
           <p><b>Street:</b> {user.address.street}</p>
           <p><b>Suite:</b> {user.address.suite}</p>
           <p><b>city:</b> {user.address.city}</p>
@@ -13,9 +36,11 @@ import React from 'react';
         </div>
     </div>
       
-    <div>
-      <p className="userName">({user.username})</p>
+    <div className="userName">
+      <p >({user.username})</p>
     </div>
   </div>
 )
+ }
+}
 export default User;
--- src/component/UserList.js
+++ src/component/UserList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import User from './User';
 const UserList = ({ users }) => {
- return ( <div className="list-container">
+ return ( <div id="list-container">
     {
       users.map((user)=>(
          <User user={user} key={user.id}/>
